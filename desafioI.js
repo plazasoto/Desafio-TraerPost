ec50ef591744115541c808929bb100ad2ea79047
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-13 10:28:53 -0400

    Consulta a API y muestra posts por consola.

--- desafioI.js
+++ desafioI.js
@@ -1,1 +1,13 @@
 console.log("(ᓀ‸ᓂ)");
+
+const getPosts = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    try{
+        const respuesta = await fetch(url);
+        const posts = await respuesta.json();
+        console.log(posts);
+    }catch(error){
+        console.error(error);
+    }
+
+}
